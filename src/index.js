6e5c2330ce310ec56479aa08f648f6875bca5dab
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2022-12-27 11:33:05 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+  id: 19231868,
+  width: 640,
+});
+
+player.on('play', function () {
+  console.log('played the video!');
+});
